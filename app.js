66ac65e78e2e734f5e722e54c88c2ff7b7fb00ee
Stuart Mackenzie <ilovediscostu@gmail.com>
2022-03-25 19:29:46 +0000

    init commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const Parkrun = require('tweaked_parkrun_js');
+const dotenv = require('dotenv');
+dotenv.config();
+
+Parkrun.auth(process.env.UNAME, process.env.PWORD, function (client, err) {
+  if (!err) {
+    // parkrun event ID for Great Field (obtained from parkrun wiki)
+    const tgfId = 2927;
+    const eventDate = '20211030';
+
+    const eventDetails = client.getRoster(tgfId, eventDate);
+    eventDetails.then(roster => {
+      const rosterCsv = [
+        [
+          '_eventNumber',
+          '_eventDate',
+          '_athleteID',
+          '_taskID',
+          '_rosterID',
+          '_taskName',
+          '_athleteFirstName',
+          '_athleteLastName',
+        ],
+        ...roster.map(item => [
+          item._eventNumber,
+          item._eventDate,
+          item._taskID,
+          item._rosterID,
+          item._taskName,
+          item._athleteFirstName,
+          item._athleteLastName,
+        ]),
+      ]
+        .map(e => e.join(','))
+        .join('\n');
+
+      // write out a file
+      fs.writeFile('./outputs/roster' + eventDate + '.csv', rosterCsv, err => {
+        console.log(err || rosterCsv.length + ' result file processed! ');
+      });
+    });
+  } else console.log(err);
+});
