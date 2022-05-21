b29dd253d7c2ff96afb844ca44c2f801ae04b022
Stuart Mackenzie <ilovediscostu@gmail.com>
2022-05-21 23:23:21 +0100

    update axios

--- allRunResults.js
+++ allRunResults.js
@@ -79,6 +79,7 @@ async function getResultData(eventNum) {
       headers: {
         'User-Agent':
           'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36',
+        'Content-Type': 'text/plain;charset=utf-8',
       },
     }
   );
